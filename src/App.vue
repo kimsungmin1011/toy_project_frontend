031d9fa278bb935b217c47d7ffd772ab2325bc63
kimsungmin1011 <tata153@naver.com>
2023-02-10 15:39:19 +0900

    my-hacker 최종본

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
+// app.vue
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <tool-bar></tool-bar> <!-- 메뉴 -->
+    <router-view></router-view> <!-- 컨텐츠 -->
+  </div>
 </template>
 
+<script>
+import ToolBar from "./components/ToolBar.vue";
+
+export default {
+  data() {
+    return {}
+  },
+  components: {ToolBar}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+body {font-family: 'Playfair Display', serif; font-size:18px; padding:0; margin:0;}
+a {color: black;}
+* {margin:0; padding:0}
+li {list-style-type: none;}
+p + p {margin-top:20px}
+#app {padding:0 20px}
+.content {padding:30px 20px}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+.list li {padding-left:10px; list-style-type: none}
+.list li + li {margin-top:20px}
+.list li > a {text-decoration: none; font-size:22px}
+.list li > b {font-style: normal; font-size:12px; color:#999}
+.list li span {font-style: normal; font-weight:700; font-size:12px; color:#999;}
+.list li i {font-style: normal; font-size:12px; color:cornflowerblue}
 </style>
